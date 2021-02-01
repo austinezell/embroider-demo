77305bf45e29a7c9cdcc5eacfb1ccbbe34cceb3a
firstname <austin.r.ezell@gmail.com>
2021-02-01 15:16:40 -0800

    refactor: enable embroider; add ie to targets; import qs

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'my-app/config/environment';
 
+import qs from 'qs';
+console.log({ qs });
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
